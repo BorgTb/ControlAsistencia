2c6ee86f642c21b56eed941bf64ab1c912b5df56
BorgTb <ameza21@alumnos.utalca.cl>
2025-09-17 13:24:25 -0300

    quitar elemento de la vista de empresa normalk

--- Frontend/src/components/components/headerEmpresa.vue
+++ Frontend/src/components/components/headerEmpresa.vue
@@ -148,7 +148,7 @@
           </router-link>
 
           <!-- Gestión de Trabajadores -->
-          <router-link
+          <router-link v-if ="esEst"
             to="/empresa/trabajadores/asociar"
             class="flex items-center space-x-2 px-3 py-4 text-sm font-medium text-gray-700 hover:text-indigo-600 hover:bg-white rounded-t-lg transition-all duration-200 whitespace-nowrap border-b-2 border-transparent hover:border-indigo-600"
             active-class="text-indigo-600 bg-white border-indigo-600"
